4f8d1825ee172b995e5c3cc2073c7edd911a32bd
Mikimiki01 <aimhh0124@gmail.com>
2023-10-31 11:17:49 +0900

    rails_bookers3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'  # get '/' => "homes#top"でも同じ意味になる
   resources :books
-  get '/books/new' => 'books#new'
-  post '/books' => 'books#create'
-  get '/books' => 'books#index'
-  get 'books/show' => 'books#show'
-  get 'books/edit' => 'books#edit'
+  # get 'books' => 'books#new'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/show' => 'books#show'
+  # get 'books/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
